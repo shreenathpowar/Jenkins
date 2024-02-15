4f855c812dc1c6cf565bdea9bab852625ad5224c
Shreenath Powar <73632896+shreenathpowar@users.noreply.github.com>
2024-02-15 18:00:11 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,9 +1,10 @@
 console.log('hello jenkins');
+
+function buildPopup() {
+	console.log("buildPopup called");
+	alert("Proceed with build?")
+};
+
 let button = document.getElementsByClassName("jenkins-button jenkins-button--primary jenkins-!-build-color");
 console.log(button);
 button.onclick = function() { buildPopup() };
-
-let buildPopup = () => {
-	console.log("buildPopup called");
-	alert("Proceed with build?")
-}
