9dac465cbd2905869fbd54927fbce39624ae9945
Shreenath Powar <73632896+shreenathpowar@users.noreply.github.com>
2024-02-15 17:46:58 +0530

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,3 +1,3 @@
 console.log('hello jenkins');
 let button = document.getElementsByClassName("jenkins-button jenkins-button--primary jenkins-!-build-color");
-button.onclick = { alert(location.hostname); };
+button.onclick = button.onlclick = function(Alert) { alert('hello'); };
